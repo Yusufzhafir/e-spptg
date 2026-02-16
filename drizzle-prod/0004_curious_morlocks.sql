1cc2564554e9e5e74b0f292bde00e71d14e73ee2
yusuf shadiq <70932237+Yusufzhafir@users.noreply.github.com>
2026-02-16 17:34:10 +0700

    add migrations

---
+++ drizzle-prod/0004_curious_morlocks.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "submissions" ADD COLUMN "luas_manual" double precision;--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "nama_kepala_desa" varchar(255);--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "juru_ukur_nama" varchar(255);--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "juru_ukur_jabatan" varchar(255);--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "juru_ukur_instansi" varchar(255);--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "juru_ukur_nomor_hp" varchar(20);
---
+++ drizzle-stag/0005_slow_solo.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "villages" ADD COLUMN "nama_kepala_desa" varchar(255);--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "juru_ukur_nama" varchar(255);--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "juru_ukur_jabatan" varchar(255);--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "juru_ukur_instansi" varchar(255);--> statement-breakpoint
+ALTER TABLE "villages" ADD COLUMN "juru_ukur_nomor_hp" varchar(20);
